1148eb72f6fba24f8dd8293ed070403017b0442c
Ilyas Hidayat Rusdy <saylihidayatrusdy@gmail.com>
2022-06-20 20:02:41 +0700

    Add Pagination

--- app/Http/Controllers/SearchController1.php
+++ app/Http/Controllers/SearchController1.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Str;
 
 class SearchController1 extends Controller
@@ -10,14 +12,10 @@ class SearchController1 extends Controller
     //
     public function search(Request $request)
     {
-        if(isset($_GET["page"])){
-            $page = (int)$_GET["page"];
+        function penghitungPaginate($banyakData, $perPage = 10)
+        {
+            return (int)(ceil($banyakData / $perPage));
         }
-        else {
-            $page = 1;
-        }
-
-        $start=$page*10-10;
 
         // contoh setelah dimasukin query ke solr
         // http://192.168.99.100:8983/solr/tugas/select?indent=true&q.op=OR&q=title_txt_en%3A%20Lagu
@@ -40,15 +38,57 @@ class SearchController1 extends Controller
             return $key;
         }
 
+        function paginate($items, $perPage = 10, $page = 1)
+        {
+            // deklarasi variable item yang akan ditampilkan
+            // sementara akan diisi seluruh item
+            // kemudian nanti akan direplace dengan array_slice
+            $itemToShow = $items;
+            $currentPage = $page;
+            //cek jika pagenya bukan page pertama
+            //set posisi startnya
+            if ($currentPage > 1) {
+                $start = ($currentPage * $perPage) - $perPage;
+            } else {
+                $start = 0;
+            }
+
+            $itemToShow = array_slice($items, $start, $perPage);
+
+            return $itemToShow;
+        }
+
         // menggabungkan input user dengan url pada apache solr
         //http://192.168.99.100:8983/solr/tugas/select?indent=true&q.op=OR&q=title_txt_id%3ALagu&rows=800&start=0
-        $url = "http://192.168.99.100:8983/solr/tugas/select?indent=true&q.op=OR&q=title_txt_id%3A" . convertSearchKey($request->search) . "&rows=800&start=" . $start;
+        $url = "http://192.168.99.100:8983/solr/core_1915101021/select?indent=true&q.op=OR&q=title_txt_id%3A" . convertSearchKey($request->search) . "&rows=800&start=0";
 
         // get kontennya
         $result = file_get_contents($url);
 
         // decode jsonnya jadi array paginate
         $hasil = json_decode($result, true);
-        return view('resultEngine', compact('hasil', 'page'));
+
+        $jumlahData = $hasil["response"]["numFound"];
+        $hasil1 = $hasil['response']['docs'];
+        $banyakHalaman = 1;
+        // set hasil yang akan tampil hanya 5
+        $perPage = 10;
+        // cek jika jumlah ditemukan pada solr > 5
+        if ($hasil['response']['numFound'] > 10) {
+
+            // hitung banyak halaman yang akan ditampilkan dari data yang ditemukan
+            $banyakHalaman = penghitungPaginate($hasil['response']['numFound'], $perPage);
+            // paginate halaman tersebut
+            $hasil1 = paginate($hasil1, $perPage, $request->halaman);
+        }
+
+        $previousSearch = convertSearchKey($request->search);
+
+        return view('resultEngine', [
+            'jumlahData' => $jumlahData,
+            'hasil1' => $hasil1,
+            'banyakHalaman' => $banyakHalaman,
+            'prevSearch' => $previousSearch,
+        ]);
     }
 }
